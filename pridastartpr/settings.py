6b4d1a6a4ed22ee69b742ff5a1b9cf6dffde4465
tihobg <66679938+tihobg@users.noreply.github.com>
2024-11-07 13:12:03 +0200

    Update settings.py
    Commit 3

--- pridastartpr/settings.py
+++ pridastartpr/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'django_project' / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
