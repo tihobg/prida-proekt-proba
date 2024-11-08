8504484a9800ab67c7b8fc3a3ca425ce6ae0de3c
Tihomir Tiankov <tiho_bg@abv.bg>
2024-11-08 15:56:14 +0200

    Commit 10

--- pridastartapp/urls.py
+++ pridastartapp/urls.py
@@ -5,6 +5,19 @@ urlpatterns = [
 
     path('', views.preeclampsia, name='preeclampsia1'),
     path('base/', views.base, name='base'),
+    path('home/', views.home, name='home'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('services/', views.services, name='services'),
+    path('news/', views.news, name='news'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('logout/', views.logout_view, name='logout'),
+    path('add_edit_patient', views.add_edit_patient, name='add_edit_patient'),
+    path('users/correlation', views.correlation, name='correlation'),
+    path('users/register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('database/', views.database, name='database'),
+
     # path('users/correlation/', views.correlation, name='correlation'),
 
 ]
--- pridastartapp/views.py
+++ pridastartapp/views.py
@@ -1,10 +1,90 @@
 from django.shortcuts import render
 from . models import Patients
 from . forms import PreeclampsiaForm
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import base64
+import io
+import urllib
+
+
+def home1(request):
+    return render(request, 'home1.html')
+
+
+def database(request):
+    return render(request, 'database.html')
+
+
+def index(request):
+    return render(request, 'web/index.html')
+
+
+def home(request):
+    return render(request, 'home.html')
 
 
 def base(request):
     return render(request, 'base.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def news(request):
+    return render(request, 'news.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+
+def logout_view(request):
+    # handle logout logic here
+    return render(request, 'home.html')
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+
+def add_edit_patient(request):
+    return render(request, 'add_edit_patient.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def correlation(request):
+    # if request.method == 'POST':
+    #     form = AuthenticationForm(data=request.POST)
+    #     if form.is_valid():
+    #         login(request, form.get_user())
+    #         return redirect('/')
+    # else:
+    #     form = AuthenticationForm()
+    return render(request, 'users/correlation.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect('/about_us')
+    else:
+        # form = UserCreationForm()
+        form = RegisterForm()
+        # form = NameForm()
+    return render(request, 'users/register.html', {'form': form})
 
 
 def preeclampsia(request):
@@ -14,36 +94,36 @@ def preeclampsia(request):
     s = request.POST.getlist('ss')
 
     # Visualization of Correlation
-    # matplotlib.use('agg')
+    matplotlib.use('agg')
     # # plt.style.use('ggplot')
-    # nx = np.arange(10, 20)
-    # ny = np.array([2, 1, 4, 5, 8, 12, 18, 25, 96, 48])
-    # xyz = np.array([[10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-    #                 [2, 1, 4, 5, 8, 12, 18, 25, 96, 48],
-    #                 [5, 3, 2, 1, 0, -2, -8, -11, -15, -16]])
-    #
-    # corr_matrix = np.corrcoef(xyz).round(decimals=2)
+    nx = np.arange(10, 20)
+    ny = np.array([2, 1, 4, 5, 8, 12, 18, 25, 96, 48])
+    xyz = np.array([[10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+                    [2, 1, 4, 5, 8, 12, 18, 25, 96, 48],
+                    [5, 3, 2, 1, 0, -2, -8, -11, -15, -16]])
+
+    corr_matrix = np.corrcoef(xyz).round(decimals=2)
     # # print(corr_matrix)
     #
-    # fig, ax = plt.subplots()
-    # im = ax.imshow(corr_matrix)
-    # im.set_clim(-1, 1)
-    # ax.grid(False)
-    # ax.xaxis.set(ticks=(0, 1, 2), ticklabels=('x', 'y', 'z'))
-    # ax.yaxis.set(ticks=(0, 1, 2), ticklabels=('x', 'y', 'z'))
-    # ax.set_ylim(2.5, -0.5)
-    # for i in range(3):
-    #     for j in range(3):
-    #         ax.text(j, i, corr_matrix[i, j], ha='center', va='center',
-    #                 color='r')
-    # cbar = ax.figure.colorbar(im, ax=ax, format='% .2f')
-    #
-    # fig = plt.gcf()
-    # buf = io.BytesIO()
-    # fig.savefig(buf, format='png')
-    # buf.seek(0)
-    # string = base64.b64encode(buf.read())
-    # url = urllib.parse.quote(string)
+    fig, ax = plt.subplots()
+    im = ax.imshow(corr_matrix)
+    im.set_clim(-1, 1)
+    ax.grid(False)
+    ax.xaxis.set(ticks=(0, 1, 2), ticklabels=('x', 'y', 'z'))
+    ax.yaxis.set(ticks=(0, 1, 2), ticklabels=('x', 'y', 'z'))
+    ax.set_ylim(2.5, -0.5)
+    for i in range(3):
+        for j in range(3):
+            ax.text(j, i, corr_matrix[i, j], ha='center', va='center',
+                    color='r')
+    cbar = ax.figure.colorbar(im, ax=ax, format='% .2f')
+
+    fig = plt.gcf()
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    url = urllib.parse.quote(string)
     #
     # # ax.plot(nx, ny, linewidth=0, marker='s', label='Data Points')
     # # plt.savefig("mygraph.png")
@@ -73,12 +153,12 @@ def preeclampsia(request):
     context1 = {
         'patients': patients,
         'key': s,
-        # 'key6': url,
+        'key6': url,
         # 'key7': form,
         'key8': form_pre,
 
     }
 
-    return render(request, 'preeclampsia.html', context1)
+    return render(request, 'preeclampsia1.html', context1)
 
 # Create your views here.
