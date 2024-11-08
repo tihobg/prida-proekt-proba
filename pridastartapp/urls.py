7dd3a94ac1bcd2f318aac3882c642a7664f84bbf
Tihomir Tiankov <tiho_bg@abv.bg>
2024-11-08 13:45:45 +0200

    Commit 9

--- pridastartapp/urls.py
+++ pridastartapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.preeclampsia, name='preeclampsia'),
+    path('', views.preeclampsia, name='preeclampsia1'),
     path('base/', views.base, name='base'),
     # path('users/correlation/', views.correlation, name='correlation'),
 
